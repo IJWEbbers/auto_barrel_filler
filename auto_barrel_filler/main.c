a53727af913cbfcfe1452b7d10efe85cb38ca3e6
IvoEbbers <ijw.ebbers@outlook.com>
2021-04-20 15:44:21 +0200

    with 1 float switch

---
+++ auto_barrel_filler/main.c
@@ -0,0 +1,42 @@
+#define F_CPU 16000000
+
+#include <avr/io.h>
+
+void floatSwitchInit(void)
+{
+	DDRD &= ~(1<<0);	//set pinD0 as input
+	PORTD |= (1<<0); //use pull-up resistor on pinD0
+	
+	DDRD &= ~(1<<1);	//set pinD1 as input
+	PORTD |= (1<<1); //use pull-up resistor on pinD1
+}
+
+void relayInit(void)
+{
+	DDRB |= (1<<1); //set pinB1 as output
+	PORTB &= ~(1<<1); //set portB1 low
+}
+
+int main(void)
+{
+	//Initialize the components
+    floatSwitchInit();
+    relayInit();
+	
+    	while (1)
+	{			
+		//if water is above required level, turn off pump
+		uint8_t floatSwitchState = PIND & 0x01;
+		if (floatSwitchState == 0)
+		{
+			PORTB &= ~(1<<PORTB1);
+			
+		}
+		//else turn on pump
+		else
+		{
+			PORTB |= (1<<PORTB1);
+		}
+	}
+}
+
