b13b832fdf6bc2e2f588693b202244d37e06ce0c
IvoEbbers <ijw.ebbers@outlook.com>
2021-05-10 12:58:27 +0200

    final

--- auto_barrel_filler/main.c
+++ auto_barrel_filler/main.c
@@ -2,6 +2,9 @@
 
 #include <avr/io.h>
 
+uint8_t top_switch_state;
+uint8_t bottom_switch_state;
+
 void floatSwitchInit(void)
 {
 	/* top sensor */
@@ -25,18 +28,17 @@ int main(void)
     floatSwitchInit();
     relayInit();
 	
-    	while (1)
+    while (1)
 	{			
 		//if water is above top level, turn off pump
-		uint8_t top_switch_state = PIND & (1<<0);
+		top_switch_state = PIND & (1<<0);
 		if (top_switch_state == 0)
 		{
 			PORTB &= ~(1<<PORTB1);	
 		}
 		//if water is below bottom level, turn on pump
-		//else turn on pump
-		uint8_t bottom_switch_state = PIND & (1<<1);
-		if (bottom_switch_state == 0)
+		bottom_switch_state = PIND & (1<<1);
+		if (bottom_switch_state == (1<<1))
 		{
 			PORTB |= (1<<PORTB1);
 		}
